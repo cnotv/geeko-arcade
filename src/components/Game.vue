161a736b7eed5b91271e204c8c633e404c0ad22c
cnotv <giuseppe.leo@suse.com>
2025-12-04 01:23:00 +0100

    Add initial model

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,38 +1,48 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { getTools, getModel } from '@webgametoolkit/threejs';
+import { onMounted, ref } from "vue";
+import { getTools, getModel } from "@webgametoolkit/threejs";
+import { updateAnimation } from "@webgametoolkit/animation";
 
 const canvas = ref(null);
-
-onMounted(async () => {
-  const { setup, animate, scene, world } = await getTools({
+const init = async () => {
+  const { setup, animate, scene, world, getDelta } = await getTools({
     stats: { init: () => {}, start: () => {}, stop: () => {} },
     route: { query: {} },
-    canvas: canvas.value
+    canvas: canvas.value,
   });
 
   await setup({
     config: {
-      camera: { position: [0, 5, 10] },
-      ground: { size: 100, color: 0x00ff00 },
+      camera: { position: [0, 5, 20] },
+      ground: { size: 10000, color: 0x559955 },
       sky: { size: 500, color: 0x87ceeb },
       lights: { directional: { intensity: 2 } },
     },
     defineSetup: async () => {
-      await getModel(scene, world, 'geeko.glb', {
-        position: [0, 0, 0],
-        scale: [0.3, 0.3, 0.3],
+      const geeko = await getModel(scene, world, "geeko.fbx", {
+        position: [0, -0.75, 0],
+        scale: [0.05, 0.05, 0.05],
+        boundary: 0.5,
         castShadow: true,
         receiveShadow: true,
+        animations: "geeko_ani.fbx",
       });
 
       animate({
         beforeTimeline: () => {},
-        timeline: [],
+        timeline: [
+          {
+            action: () => {
+              updateAnimation(geeko.mixer, geeko.actions["Walk"], getDelta(), 4);
+            },
+          },
+        ],
       });
     },
   });
-});
+};
+
+onMounted(async () => init());
 </script>
 
 <template>
