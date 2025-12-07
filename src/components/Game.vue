27a8fb2a687b16dc1f30de86eeb0c238d8552b50
cnotv <giuseppe.leo@suse.com>
2025-12-07 23:09:43 +0100

    Add TS to the project

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,11 @@
-<script setup>
-import { onMounted, ref } from "vue";
+<script setup lang="ts">
+import { onMounted, ref, type Ref } from "vue";
 import { getTools, getModel } from "@webgametoolkit/threejs";
-import { controllerForward, controllerTurn } from "@webgametoolkit/animation";
+import { AnimatedComplexModel, ComplexModel, controllerForward, controllerTurn } from "@webgametoolkit/animation";
 import { chameleonConfig, setupConfig, playerConfig, botConfig } from "../config";
 import { rombusGenerator } from "../utils/generators";
 
-let currentAnimation = "Idle_A";
+let currentAnimation: string = "Idle_A";
 
 /**
  * Define block positions using a matrix
@@ -22,25 +22,25 @@ let currentAnimation = "Idle_A";
   ]
  */
 const positions = Array.from(rombusGenerator(10));
-const blockSize = 2;
+const blockSize: number = 2;
 
-const checkColliders = (model, blocks) => {
+const checkColliders = (model: AnimatedComplexModel, blocks: ComplexModel[]): void => {
 
 };
 
-const moveModel = (model, delta, blocks, angle) => {
+const moveModel = (model: AnimatedComplexModel, delta: number, blocks: ComplexModel[], angle: number): void => {
   controllerTurn(model, angle);
-  controllerForward(model, [], blockSize, delta)
+  controllerForward(model, [], blockSize, delta);
 };
 
-const getAngle = () => {
+const getAngle = (): number => {
   // Return a random angle between -1 and 1
   return (Math.random() * 2 - 1) * 0.05;
 };
 
-const canvas = ref(null);
-const init = async () => {
-  const { setup, animate, scene, world, getDelta } = await getTools({ canvas: canvas.value });
+const canvas: Ref<HTMLCanvasElement | null> = ref(null);
+const init = async (): Promise<void> => {
+  const { setup, animate, scene, world, getDelta } = await getTools({ canvas: canvas.value! });
   await setup({
     config: setupConfig,
     defineSetup: async () => {
@@ -57,8 +57,8 @@ const init = async () => {
           {
             frequency: 10,
             action: () => {
-              const playerAngle = getAngle();
-              const botAngle = getAngle();
+              const playerAngle: number = getAngle();
+              const botAngle: number = getAngle();
               // moveModel(player, getDelta(), blocks, blockSize, playerAngle);
               // moveModel(bot, getDelta(), blocks, blockSize, botAngle);
               checkColliders(player, blocks);
