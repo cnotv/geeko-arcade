cbb31e5da31877b5b39fdcca2b785eb7b00128bc
cnotv <giuseppe.leo@suse.com>
2025-12-08 16:45:38 +0100

    Add Sentry

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import * as Sentry from "@sentry/vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
+app.mount("#app");
+
